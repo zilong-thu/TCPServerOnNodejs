3702f60a91ef7ea7fc3144f4dc501afa9b28fef5
王子龙 <809052335@qq.com>
2015-09-01 20:42:06 +0800

    add client.js

---
+++ client.js
@@ -0,0 +1,35 @@
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+var CONCURRENCY = 4500;
+
+for (var i = 0; i < CONCURRENCY; i++) {
+	(function(net){
+		var iter = i;
+		var msg  = 'client '+ iter + ' said hello.';
+
+		var client = net.connect({
+			port: PORT,
+			host: HOST
+		},function() { //'connect' listener
+		  console.log('client ', iter, ' connected to server!');
+		  client.write(msg);
+		});
+
+		client.on('data', function(data) {
+		    console.log('Iterval ', iter, ': ', data.toString());
+		    var timeToWait = (200 + Math.random() * 800 ).toFixed(0);
+
+		  	setTimeout(function(){
+		  		client.write(msg);
+		  	}, timeToWait);
+
+		});
+
+		client.on('end', function() {
+		  console.log(iter, 'disconnected from server');
+		});
+	})(net);
+}
