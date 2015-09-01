23945fe921093dd3b3e12b8d34fab106a08e85b4
zilong-thu <809052335@qq.com>
2015-09-01 20:46:18 +0800

    small updates

--- client.js
+++ client.js
@@ -3,19 +3,33 @@ var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 6969;
 
-var client = net.connect({
-	port: PORT,
-	host: HOST
-},function() { //'connect' listener
-  console.log('connected to server!');
-  client.write('world!\r\n');
-});
+var CONCURRENCY = 4500;
 
-client.on('data', function(data) {
-  console.log(data.toString());
-  client.end();
-});
+for (var i = 0; i < CONCURRENCY; i++) {
+	(function(net){
+		var iter = i;
+		var msg  = 'client '+ iter + ' said hello.';
 
-client.on('end', function() {
-  console.log('disconnected from server');
-});
+		var client = net.connect({
+			port: PORT,
+			host: HOST
+		},function() { //'connect' listener
+		  console.log('client ', iter, ' connected to server!');
+		  client.write(msg);
+		});
+
+		client.on('data', function(data) {
+		    console.log('Iterval ', iter, ': ', data.toString());
+		    var timeToWait = (200 + Math.random() * 800 ).toFixed(0);
+
+		  	setTimeout(function(){
+		  		client.write(msg);
+		  	}, timeToWait);
+
+		});
+
+		client.on('end', function() {
+		  console.log(iter, 'disconnected from server');
+		});
+	})(net);
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var net = require('net');
+var moment = require('moment');
 
 var HOST = '127.0.0.1';
 var PORT = 6969;
@@ -12,20 +13,26 @@ net.createServer(function(sock) {
     var remotePort    = sock.remotePort;
 
     // 我们获得一个连接 - 该连接自动关联一个socket对象
-    console.log('CONNECTED: ' +
-        remoteAddress + ':' + remotePort);
+    console.log('CONNECTED: ' + remoteAddress + ':' + remotePort);
 
     // 为这个socket实例添加一个"data"事件处理函数
     sock.on('data', function(data) {
-        console.log('DATA ' + remoteAddress + ': ' + data);
-        // 回发该数据，客户端将收到来自服务端的数据
-        sock.write('Data : "' + data + '"');
+        console.log(moment().format() + ', ' + remoteAddress + ': ' +  remotePort + ' : ' + data);
+
+        // 过若干时间后回发该数据，客户端将收到来自服务端的数据
+        setTimeout(function(){
+            sock.write('Data : "' + data + '"');
+        }, 50);
     });
 
     // 为这个socket实例添加一个"close"事件处理函数
     sock.on('close', function(data) {
         console.log('CLOSED: ' +
             remoteAddress + ' ' + remotePort);
+    });
+
+    sock.on('error', function(error){
+        console.log('Error occured.');
     });
 
 }).listen(PORT, HOST);
